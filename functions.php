2f51a612bdb47dea84cb8010f042cde8fea2a72b
xxHiPowerxx <rickyadams432@gmail.com>
2021-07-02 13:12:14 -0700

    added Categories to Search Results

--- functions.php
+++ functions.php
@@ -12,5 +12,43 @@ function salient_child_enqueue_styles() {
 		}
 }
 
+/**
+ * Search For Terms - Searches database for Specified Term
+ * 
+ * @param string - $term The Specified Term EG: Category
+ * @return array|false - Returns Array of found Terms or False
+ */
+function search_for_terms( $term ) {
+	// Use search term and cross-referrence Categories as well.
+	$search_term = explode( ' ', get_search_query( false ) );
+	global $wpdb;
+	$select = "
+	SELECT DISTINCT t.*, tt.* 
+	FROM wp_terms AS t 
+	INNER JOIN wp_term_taxonomy AS tt 
+	ON t.term_id = tt.term_id 
+	WHERE tt.taxonomy IN ('$term')";
+
+	$first = true;
+	foreach ( $search_term as $s ) :
+		if ( $first ) :
+			$select .= " AND (t.name LIKE '%s')";
+			$string_replace[] = '%'.$wpdb->esc_like( $s ).'%';
+			$first            = false;
+		else :
+			$select .= " OR (t.name LIKE '%s')";
+			$string_replace[] = '%'. $wpdb->esc_like( $s ).'%';
+		endif;
+	endforeach; // endforeach ( $search_term as $s ) :
+	$select .= " ORDER BY t.name ASC";
+	$terms       = $wpdb->get_results( $wpdb->prepare( $select, $string_replace ) );
+	if ( count( $terms ) > 0 ) :
+		add_filter( 'body_class', function( $classes ) {
+			return str_replace( 'search-no-results', 'search-results', $classes );
+		} );
+	endif;
+
+	return $terms;
+}
 
 ?>
